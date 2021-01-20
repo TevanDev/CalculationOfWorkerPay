0cd4a17a3ad53d8a679a64bb509c89d813ee317b
TevanDev <77395591+TevanDev@users.noreply.github.com>
2021-01-20 18:12:39 +0200

    Add project files.

---
+++ WorkerPay/Pay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerPay
+{
+    class Pay
+    {
+        double pay;
+        double tax = 0.14;
+        public double tPay(double numH, double pPerH)
+
+        {
+            pay = (numH * pPerH) - ((numH * pPerH) * tax);
+
+            return pay;
+        }
+    }
+}
---
+++ WorkerPay/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace WorkerPay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numH = 0;
+            bool count = false;
+            double pay = 0;
+            bool repeat = false;
+            var cont = "";
+            Pay obj = new Pay();
+            while (repeat == false)
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Enter name of Worker");
+                var name = Console.ReadLine();
+
+
+
+                while (count == false)
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter the number of hours " + name + " worked");
+                        numH = double.Parse(Console.ReadLine());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("The value you have entered is invalid");
+                        count = true;
+                    }
+
+
+                    if (count == false)
+                    {
+                        try
+                        {
+                            Console.WriteLine("Enter the pay " + name + " gets per hour");
+                            pay = double.Parse(Console.ReadLine());
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("The number of hours is invalid");
+                            count = true;
+                        }
+                    }
+
+
+                    count = true;
+
+                }
+                Console.WriteLine(name + " has earned R" + obj.tPay(numH, pay));
+
+                Console.WriteLine("Would you like to calculate another worker's pay? (y/n) ");
+                cont = Console.ReadLine();
+                if (cont == "n" || cont == "N")
+                {
+                    repeat = true;
+                }
+                else if (cont == "Y" || cont == "y")
+                {
+                    repeat = false;
+                    count = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid entry, Program is starting again. ");
+                }
+
+
+
+            }
+
+        }
+    }
+}
